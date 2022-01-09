3e100ec3fe07381616b385146c3131e5743601ff
quangpn0204 <19522100@gm.uit.edu.vn>
2022-01-09 16:30:56 +0700

    fix import

--- Import_ExportService/src/ImportService.Infrastructure/Repositories/ImportRepository.cs
+++ Import_ExportService/src/ImportService.Infrastructure/Repositories/ImportRepository.cs
@@ -23,39 +23,51 @@ namespace ImportService.Infrastructure.Repositories
             // modified here to handle import 
             
             var dbImportFile = _mapper.Map<Entities.ImportFile>(importFile);
-            for (int i = 0; i < objs; i++)
-			{
                 switch(importFile.ObjectImported == 'Product')
                 {
                     case 'Product':
+                     for (int i = 0; i < objs.count; i++)
+                     {
                         Product product = new Product();
                         //convert objs[i] => product at here
                         _dbContext.Products.Add(product);
+                     }    
                         break;
                     case 'Attribute':
+                     for (int i = 0; i < objs.count; i++)
+                     {
                         Attribute attribute = new Attribute();
                         //convert objs[i] => attribute at here
                         _dbContext.Attributes.Add(new Attribute());
                         break;
+                     }
                     case 'Category':
+                     for (int i = 0; i < objs.count; i++)
+                     {
                         Category category = new Category();
                          //convert objs[i] => category at here
                         _dbContext.Categorys.Add(category);
                         break;
+                     }
                     case 'Channel':
+                     for (int i = 0; i < objs.count; i++)
+                     {
                         Channel channel = new Channel();
                          //convert objs[i] => channel at here
                         _dbContext.Channels.Add(channel);
                         break;
+                     }
                     case 'Supplier':
+                     for (int i = 0; i < objs.count; i++)
+                     {
                         Suppiler suppiler = new Suppiler();
                          //convert objs[i] => suppiler at here
                         _dbContext.Suppliers.Add(suppiler);
                         break;
+                     }
                     default:
                         break;
                 }   
-			}
             await _dbContext.SaveChangesAsync();
             return true;
         }
